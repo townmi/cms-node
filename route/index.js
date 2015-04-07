7092601e7b079299d373e4cfda12792e26724022
townmi <towne766@126.com>
2015-04-07 15:44:33 +0800

    add last

--- route/index.js
+++ route/index.js
@@ -12,7 +12,7 @@ var pool = mysql.createPool({
 	host : config.host,
 	port : config.port,
 	user : config.user,
-	database : 'test',
+	database : 'cms',
 	password : config.password,
 	multipleStatements : true
 })
@@ -20,62 +20,6 @@ var pool = mysql.createPool({
 
 router.get("/", function (req, res){
 
-	pool.getConnection(function (err, connection) {
-
-		connection.query('SELECT * FROM pro ; SELECT * FROM excel', function (err, rows) {
-
-			connection.release();
-
-			console.log(rows);
-
-			res.render("index",{"rows" : rows});
-
-		});
-
-	});
-
-})
-
-// delete
-router.post('/delete', function (req, res){
-
-	pool.getConnection(function (err, connection) {
-
-		connection.query("DELETE FROM pro WHERE id = "+req.body.del, function (err, rows) {
-
-			connection.release();
-
-			// res.redirect(301,"/");
-
-			res.send({"target" : true});
-
-		});
-
-	});
+	
 
 });
-
-// tijiao
-router.post('/post', function (req, res){
-
-	var STR = '"'+req.body.name +'","'+ req.body.price +'","'+ req.body.number +'","'+ req.body.date +'","'+ req.body.pro_u +'","'+ req.body.user+'"';
-
-	pool.getConnection(function (err, connection) {
-
-		// 'INSERT INTO test(name, sex) values("hanmeimei", "1"),("lilie", "0")'
-
-		connection.query('INSERT INTO pro(name, price, number, date, pro_u, user) values('+STR+')', function (err, rows) {
-
-			connection.release();
-
-			// res.redirect(301,"/");
-
-			res.send({"target" : true});
-
-		});
-
-	});
-
-});
-
-
